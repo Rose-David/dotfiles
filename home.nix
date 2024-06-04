e449957a3a8c7721de2758369b2de1d4c136a794
David Rose <david.rose.jack@gmail.com>
2024-06-04 12:20:58 -0400

    Git init

---
+++ home.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "davidr";
+  home.homeDirectory = "/home/davidr";
+
+  # don't change this:
+  home.stateVersion = "24.05";
+
+  # Let home manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Enable and configure Neovim:
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
+    plugins = with pkgs.vimPlugins; [
+      mason-nvim
+    ];
+  };
+}
