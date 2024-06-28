f004e37f28b9cda721b2be5655293e3225edac74
David Rose <david.rose.jack@gmail.com>
2024-06-27 21:22:42 -0400

    Changed wine version

--- configuration.nix
+++ configuration.nix
@@ -130,7 +130,7 @@
     betaflight-configurator inav-configurator
 
     # misc creative:
-    gimp libreoffice krita inkscape obs-studio blender-hip freecad wineWowPackages.wayland yad winetricks
+    gimp libreoffice krita inkscape obs-studio blender-hip freecad wineWowPackages.staging yad winetricks
 
     # files:
     pika-backup warp
