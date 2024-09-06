b18fc4d6f18d61b23eaee656abf664ade1bd65b9
David Rose <david.rose.jack@gmail.com>
2024-09-06 15:41:52 -0400

    is this it now?

--- nvim/plugin/rustacean.lua
+++ nvim/plugin/rustacean.lua
@@ -24,7 +24,7 @@ vim.g.rustaceanvim = {
 
       bufmap('K', vim.lsp.buf.hover)
 
-      bufmap('<Leader>e', "<cmd>RustLsp explainError<CR>")
+      bufmap('<Leader>e', "<cmd>RustLsp explainError<CR>", { desc = "Explain Error" })
 
       vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
         vim.lsp.buf.format()
@@ -40,15 +40,3 @@ vim.g.rustaceanvim = {
   dap = {
   },
 }
-
-vim.keymap.set('n', "<Leader>di", "<cmd>lua require('dap').step_into()<CR>", { desc = "Debugger step into" }) 
-vim.keymap.set('n', "<Leader>dov", "<cmd>lua require('dap').step_over()<CR>", { desc = "Debugger step over" }) 
-vim.keymap.set('n', "<Leader>dou", "<cmd>lua require('dap').step_out()<CR>", { desc = "Debugger step out" })
-vim.keymap.set('n', "<Leader>dc", "<cmd>lua require('dap').continue()<CR>", { desc = "Debugger continue" })
-vim.keymap.set('n', "<Leader>db", "<cmd>lua require('dap').toggle_breakpoint()<CR>", { desc = "Debugger toggle breakpoint" })
-vim.keymap.set('n', "<Leader>dd", "<cmd>lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", { desc = "Debugger set conditional breakpoint" })
-vim.keymap.set('n', "<Leader>de", "<cmd>lua require('dap').terminate()<CR>", { desc = "Debugger reset" })
-vim.keymap.set('n', "<Leader>dr", "<cmd>lua require('dap').run_last()<CR>", { desc = "Debugger run last" })
--- rustaceanvim
-vim.keymap.set('n', "<Leader>dt", "<cmd>lua vim.cmd('RustLsp testables')<CR>", { desc = "Debugger testables" })
-
