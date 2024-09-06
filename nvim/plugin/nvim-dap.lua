532145903fe84e527e1a8c07fbf497f8875faf7e
David Rose <david.rose.jack@gmail.com>
2024-09-06 15:07:47 -0400

    Fixed lldb location

--- nvim/plugin/nvim-dap.lua
+++ nvim/plugin/nvim-dap.lua
@@ -20,7 +20,7 @@ end
 
 dap.adapters.lldb = {
   type = 'executable',
-  command = '/usr/bin/lldb-dap', -- adjust as needed, must be absolute path
+  command = '/run/current-system/sw/bin/lldb-dap', -- adjust as needed, must be absolute path
   name = 'lldb'
 }
 
