737ab185f50e03564afe0e971e5e190f5552352e
David Rose <david.rose.jack@gmail.com>
2024-09-06 18:07:03 -0400

    Fixed missing parentheses

--- nvim/options.lua
+++ nvim/options.lua
@@ -15,7 +15,7 @@ vim.keymap.set('n', "<Leader>dr", "<cmd>lua require('dap').run_last()<CR>", { de
 vim.keymap.set('n', "<Leader>dt", "<cmd>lua vim.cmd('RustLsp testables')<CR>", { desc = "Debugger testables" })
 -- Telescope
 local builtin = require("telescope.builtin")
-vim.keymap.set('n', "<leader>ff", builtin.find_files()
+vim.keymap.set('n', "<leader>ff", builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
