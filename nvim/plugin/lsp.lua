d2b185ffa10fdba98e5a1eb840393c4f77a27be5
David Rose <david.rose.jack@gmail.com>
2024-09-06 09:44:43 -0400

    One last try

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -18,6 +18,21 @@ local on_attach = function(_, bufnr)
 
   bufmap('K', vim.lsp.buf.hover)
 
+-- keybinds setup
+  
+  local map = vim.keymap.set
+
+  map('n', "<Leader>di", "<cmd>lua require('dap').step_into()<CR>", { desc = "Debugger step into", buffer = bufnr }) 
+  map('n', "<Leader>dov", "<cmd>lua require('dap').step_over()<CR>", { desc = "Debugger step over", buffer = bufnr  }) 
+  map('n', "<Leader>dou", "<cmd>lua require('dap').step_out()<CR>", { desc = "Debugger step out", buffer = bufnr  })
+  map('n', "<Leader>dc", "<cmd>lua require('dap').continue()<CR>", { desc = "Debugger continue", buffer = bufnr  })
+  map('n', "<Leader>db", "<cmd>lua require('dap').toggle_breakpoint()<CR>", { desc = "Debugger toggle breakpoint", buffer = bufnr  })
+  map('n', "<Leader>dd", "<cmd>lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", { desc = "Debugger set conditional breakpoint", buffer = bufnr  })
+  map('n', "<Leader>de", "<cmd>lua require('dap').terminate()<CR>", { desc = "Debugger reset", buffer = bufnr  })
+  map('n', "<Leader>dr", "<cmd>lua require('dap').run_last()<CR>", { desc = "Debugger run last", buffer = bufnr  })
+          -- rustaceanvim
+  map('n', "<Leader>dt", "<cmd>lua vim.cmd('RustLsp testables')<CR>", { desc = "Debugger testables", buffer = bufnr  })
+
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
   end, {})
