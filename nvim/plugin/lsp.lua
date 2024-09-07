6de7651d04afa489b3ef9836e1cc6f1421e655be
David Rose <david.rose.jack@gmail.com>
2024-09-07 18:01:48 -0400

    Fixed error resulting in missing descs

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -1,20 +1,22 @@
 local on_attach = function(_, bufnr)
 
   local bufmap = function(keys, func, opts)
+    opts = opts or {}
+    opts.buffer = bufnr
     vim.keymap.set('n', keys, func, opts)
   end
 
-  bufmap('<leader>r', vim.lsp.buf.rename, { buffer = bufnr, desc = "Rename Symbol" } )
-  bufmap('<leader>a', vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code Action" })
+  bufmap('<leader>r', vim.lsp.buf.rename, { desc = "Rename Symbol" } )
+  bufmap('<leader>a', vim.lsp.buf.code_action, { desc = "Code Action" })
 
-  bufmap('gd', vim.lsp.buf.definition, { buffer = bufnr, desc = "Go to Definition" })
-  bufmap('gD', vim.lsp.buf.declaration, { buffer = bufnr, desc = "Go to Declaration" })
-  bufmap('gI', vim.lsp.buf.implementation, { buffer = bufnr, desc = "Go to Implementation" } )
-  bufmap('<leader>D', vim.lsp.buf.type_definition { buffer = bufnr, desc = "Type Definition" } )
+  bufmap('gd', vim.lsp.buf.definition, { desc = "Go to Definition" })
+  bufmap('gD', vim.lsp.buf.declaration, { desc = "Go to Declaration" })
+  bufmap('gI', vim.lsp.buf.implementation, { desc = "Go to Implementation" } )
+  bufmap('<leader>D', vim.lsp.buf.type_definition { desc = "Type Definition" } )
 
-  bufmap('gr', require('telescope.builtin').lsp_references, { buffer = bufnr, desc = "Search References" } )
-  bufmap('<leader>s', require('telescope.builtin').lsp_document_symbols { buffer = bufnr, desc = "Search Document Symbols" })
-  bufmap('<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols, { buffer = bufnr, desc = "Search Dynamic Workspace Symbols" })
+  bufmap('gr', require('telescope.builtin').lsp_references, { desc = "Search References" } )
+  bufmap('<leader>s', require('telescope.builtin').lsp_document_symbols { desc = "Search Document Symbols" })
+  bufmap('<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols, { desc = "Search Dynamic Workspace Symbols" })
 
   bufmap('K', vim.lsp.buf.hover)
 
