445af47634a13924437b0ceb7cdc48407eeb3bf3
David Rose <david.rose.jack@gmail.com>
2024-09-07 21:20:56 -0400

    Added LaTeX grouping to which-key

--- nvim/plugin/which-key.lua
+++ nvim/plugin/which-key.lua
@@ -3,5 +3,6 @@ wk.add({
 	{ "<leader>c", group = 'Cargo' },
 	{ "<leader>d", group = "Debugger" },
 	{ "<leader>h", group = "Git" },
-	{ "<leader>f", group = "Find" }
+	{ "<leader>f", group = "Find" },
+	{ "<leader>l", group = "LaTeX" }
 })
