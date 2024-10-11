c0328241a6f29eeb82c49dbd15e8349e512c8b1a
David Rose <david.rose.jack@gmail.com>
2024-10-10 20:53:25 -0400

    Fixed error

--- nvim/options.lua
+++ nvim/options.lua
@@ -30,7 +30,9 @@ vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find Help Tags" })
 -- Telescope and Project
-vim.keymap.set("n", "<leader>fp", require("telescope").extensions.projects.projects({}), { desc = "Find Help Tags" })
+vim.keymap.set("n", "<leader>fp", function()
+	require("telescope").extensions.projects.projects({})
+end, { desc = "Find Help Tags" })
 -- Conform
 vim.api.nvim_create_user_command("Format", function()
 	require("conform").format()
