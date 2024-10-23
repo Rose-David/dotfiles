6d131e125d8d109e8541c60007d49d2489868c3f
David Rose <david.rose.jack@gmail.com>
2024-10-23 18:26:51 -0400

    this time fo sho

--- flake.nix
+++ flake.nix
@@ -11,6 +11,7 @@
     nixosConfigurations = {
       nixos-desktop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+        specialArgs = {inherit inputs;};
         modules = [
           ./configuration.nix
           home-manager.nixosModules.home-manager
